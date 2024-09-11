6dfbe639c3e47bffd189e9e3cf8ec4e9962192e9
panch0r3d <47708974+panch0r3d@users.noreply.github.com>
2024-09-10 20:22:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Function to fetch data from the API
 async function fetchData() {
     try {
-        const response = await fetch('http://ip-api.com/json/');
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
         const data = await response.json();
         return data;
     } catch (error) {
