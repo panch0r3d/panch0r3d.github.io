0b6c55550763887a05ad86735dcc70e867b36b71
panch0r3d <47708974+panch0r3d@users.noreply.github.com>
2024-09-10 20:01:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 fetch(‘http://ip-api.com/json/’)
    .then(response => response.json())
    .then(IPinfo => showIPinfo(IPinfo.results));
+
+showIPinfo = IPinfo => {
+  const IPinfoDiv = document.querySelector(‘#ipinformation’);
+  IPinfos.forEach(IPinfo => {
+    const IPinfoElement = document.createElement(‘p’);
+    IPinfoElement.innerText = `IP Address: ${IPinfo.query}`;
+    IPinfoDiv.append(IPinfoElement);
+  });
+}
